a13a880e24c2b3a96ca2a70d88fbfba203a4d87c
jeran <jerantfox@gmail.com>
2023-02-04 14:43:56 -0500

    Reproduce warning
    - Add ktor client dependency
    - Enable minify
    - Use ktor client so it isn't stripped
    
    Running `./gradlew assembleRelease` will produce the following warning:
    
    WARNING:R8: Missing class org.slf4j.impl.StaticLoggerBinder (referenced from: void org.slf4j.LoggerFactory.bind() and 3 other contexts)

--- app/src/main/java/dev/jeran/ktorr8/MainActivity.kt
+++ app/src/main/java/dev/jeran/ktorr8/MainActivity.kt
@@ -1,11 +1,30 @@
 package dev.jeran.ktorr8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CoroutineScope(Dispatchers.Main).launch { useKtor() }
+    }
+
+    private suspend fun useKtor() {
+        HttpClient().use { client ->
+            client.get("https://pokeapi.co/api/v2/pokemon/1/") {
+                contentType(ContentType.Application.Json)
+            }
+        }.bodyAsText().let { body ->
+            println(body)
+        }
     }
 }
